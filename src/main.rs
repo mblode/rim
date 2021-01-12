65647f3d9f02b3215b50b055a5e06d6e644f7801
Matthew Blode <m@blode.co>
2021-01-12 19:04:25 +1100

    Search and Syntax

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,16 @@
 )]
 mod document;
 mod editor;
+mod filetype;
+mod highlighting;
 mod row;
 mod terminal;
 pub use document::Document;
 use editor::Editor;
 pub use editor::Position;
+pub use editor::SearchDirection;
+pub use filetype::FileType;
+pub use filetype::HighlightingOptions;
 pub use row::Row;
 pub use terminal::Terminal;
 
--- src/terminal.rs
+++ src/terminal.rs
@@ -9,7 +9,6 @@ pub struct Size {
     pub width: u16,
     pub height: u16,
 }
-
 pub struct Terminal {
     size: Size,
     _stdout: RawTerminal<std::io::Stdout>,
@@ -18,7 +17,6 @@ pub struct Terminal {
 impl Terminal {
     pub fn default() -> Result<Self, std::io::Error> {
         let size = termion::terminal_size()?;
-
         Ok(Self {
             size: Size {
                 width: size.0,
@@ -27,16 +25,14 @@ impl Terminal {
             _stdout: stdout().into_raw_mode()?,
         })
     }
-
     pub fn size(&self) -> &Size {
         &self.size
     }
-
     pub fn clear_screen() {
         print!("{}", termion::clear::All);
     }
 
-    #[allow(clippy::clippy::cast_possible_truncation)]
+    #[allow(clippy::cast_possible_truncation)]
     pub fn cursor_position(position: &Position) {
         let Position { mut x, mut y } = position;
         x = x.saturating_add(1);
@@ -45,11 +41,9 @@ impl Terminal {
         let y = y as u16;
         print!("{}", termion::cursor::Goto(x, y));
     }
-
     pub fn flush() -> Result<(), std::io::Error> {
         io::stdout().flush()
     }
-
     pub fn read_key() -> Result<Key, std::io::Error> {
         loop {
             if let Some(key) = io::stdin().lock().keys().next() {
@@ -57,31 +51,24 @@ impl Terminal {
             }
         }
     }
-
     pub fn cursor_hide() {
         print!("{}", termion::cursor::Hide);
     }
-
     pub fn cursor_show() {
         print!("{}", termion::cursor::Show);
     }
-
     pub fn clear_current_line() {
         print!("{}", termion::clear::CurrentLine);
     }
-
     pub fn set_bg_color(color: color::Rgb) {
         print!("{}", color::Bg(color));
     }
-
     pub fn reset_bg_color() {
         print!("{}", color::Bg(color::Reset));
     }
-
     pub fn set_fg_color(color: color::Rgb) {
         print!("{}", color::Fg(color));
     }
-
     pub fn reset_fg_color() {
         print!("{}", color::Fg(color::Reset));
     }
